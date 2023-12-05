fde44e48640d5013a8b490690eb68c3038c14545
m21wx <126360340+m21wx@users.noreply.github.com>
2023-12-05 20:19:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+			document.querySelector('.close').addEventListener('click',()=>{
+				document.querySelector('.boxNav').classList.toggle('active')
+			})
+			document.querySelector('.close-2').addEventListener('click',()=>{
+				document.querySelector('.boxNav').classList.remove('active')
+			})
+			
